f1fa1e780269602d8850b9804ad34669723b59f5
Daniel Mundra <daniel.mundra@civicactions.com>
2021-11-18 10:24:29 -0800

    Fixed report filename build construction.

--- src/components/report/ReportYAMLDownload.svelte
+++ src/components/report/ReportYAMLDownload.svelte
@@ -4,11 +4,19 @@
 
   $evaluation.title = $evaluation['product']['name'] + " " + $evaluation.title;
 
+  var reportFilename = "report";
+  if ($evaluation['product']['name']) {
+    reportFilename = $evaluation['product']['name'].toLowerCase();
+  }
+  if ($evaluation['product']['version']) {
+    reportFilename += "-" + $evaluation['product']['version'];
+  }
+
   $: yamlDownload = `data:application/yaml;charset=utf-8,${encodeURIComponent(
     yaml.dump($evaluation)
   )}`;
 </script>
 
-<a href={yamlDownload} download="{$evaluation['product']['name'].toLowerCase()}-{$evaluation['product']['version']}.yaml" class="button">
+<a href={yamlDownload} download="{reportFilename}.yaml" class="button">
   Download Report (YAML)
 </a>
