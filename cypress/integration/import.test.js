60482fc4aeebc33bdf8810176e2ac947c0e96571
Daniel Mundra <daniel.mundra@civicactions.com>
2021-12-30 08:20:08 -0800

    Added govready to import example.

--- cypress/integration/import.test.js
+++ cypress/integration/import.test.js
@@ -1,59 +1,96 @@
 /// <reference types="Cypress" />
 
 describe("Import", () => {
-  it("should import drupal-9.yaml example", () => {
-    const drupalYAMLExample = "drupal-9.yaml";
-    const fileType = "application/x-yaml";
-    cy.fixture(drupalYAMLExample).as("drupalExampleFixture");
+  const yamlExamples = [
+    {
+      filename: "drupal-9.yaml",
+      reportname: "Drupal",
+      total: 183,
+      success_criteria_level_a: 100,
+      success_criteria_level_aa: 48,
+      success_criteria_level_aaa: 22,
+      functional_performance_criteria: 9,
+      hardware: 0,
+      software: 0,
+      support_documentation_and_services: 4,
+    },
+    {
+      filename: "govready-0.9.yaml",
+      reportname: "GovReady",
+      total: 160,
+      success_criteria_level_a: 100,
+      success_criteria_level_aa: 47,
+      success_criteria_level_aaa: 0,
+      functional_performance_criteria: 9,
+      hardware: 0,
+      software: 0,
+      support_documentation_and_services: 4,
+    },
+  ];
 
-    cy.visit("/");
+  yamlExamples.forEach((yamlExample) => {
+    it(`should import ${yamlExample.filename} example`, () => {
+      const fileType = "application/x-yaml";
+      cy.fixture(yamlExample.filename).as("yamlFixture");
 
-    cy.get('input[type="file"]').then(function ($input) {
-      const blob = Cypress.Blob.binaryStringToBlob(
-        this.drupalExampleFixture,
-        fileType
+      cy.visit("/");
+
+      cy.get('input[type="file"]').then(function ($input) {
+        const blob = Cypress.Blob.binaryStringToBlob(
+          this.yamlFixture,
+          fileType
+        );
+        const file = new File([blob], yamlExample.filename, { type: fileType });
+        const list = new DataTransfer();
+
+        list.items.add(file);
+        const myFileList = list.files;
+
+        $input[0].files = myFileList;
+        $input[0].dispatchEvent(new Event("change", { bubbles: true }));
+      });
+
+      cy.on("window:alert", (txt) => {
+        expect(txt).to.contain(`OpenACR "${yamlExample.reportname}" loaded`);
+      });
+
+      cy.get("h2.your-report__heading").should(
+        "contain",
+        `Report for ${yamlExample.reportname}`
       );
-      const file = new File([blob], drupalYAMLExample, { type: fileType });
-      const list = new DataTransfer();
 
-      list.items.add(file);
-      const myFileList = list.files;
+      cy.get(".your-report__description").should(
+        "contain",
+        `Reported on\n  ${yamlExample.total}\n  of\n  339\n  Total Criteria.`
+      );
 
-      $input[0].files = myFileList;
-      $input[0].dispatchEvent(new Event("change", { bubbles: true }));
+      cy.get(
+        ".progress__principle a[href$='/chapter/success_criteria_level_a'] + .progress__part"
+      ).should("contain", `${yamlExample.success_criteria_level_a} of 100`);
+      cy.get(
+        ".progress__principle a[href$='/chapter/success_criteria_level_aa'] + .progress__part"
+      ).should("contain", `${yamlExample.success_criteria_level_aa} of 52`);
+      cy.get(
+        ".progress__principle a[href$='/chapter/success_criteria_level_aaa'] + .progress__part"
+      ).should("contain", `${yamlExample.success_criteria_level_aaa} of 92`);
+      cy.get(
+        ".progress__principle a[href$='/chapter/functional_performance_criteria'] + .progress__part"
+      ).should(
+        "contain",
+        `${yamlExample.functional_performance_criteria} of 9`
+      );
+      cy.get(
+        ".progress__principle a[href$='/chapter/hardware'] + .progress__part"
+      ).should("contain", `${yamlExample.hardware} of 55`);
+      cy.get(
+        ".progress__principle a[href$='/chapter/software'] + .progress__part"
+      ).should("contain", `${yamlExample.software} of 26`);
+      cy.get(
+        ".progress__principle a[href$='/chapter/support_documentation_and_services'] + .progress__part"
+      ).should(
+        "contain",
+        `${yamlExample.support_documentation_and_services} of 5`
+      );
     });
-
-    cy.on("window:alert", (txt) => {
-      expect(txt).to.contain('OpenACR "Drupal" loaded');
-    });
-
-    cy.get("h2.your-report__heading").should("contain", "Report for Drupal");
-
-    cy.get(".your-report__description").should(
-      "contain",
-      "Reported on\n  183\n  of\n  339\n  Total Criteria."
-    );
-
-    cy.get(
-      ".progress__principle a[href$='/chapter/success_criteria_level_a'] + .progress__part"
-    ).should("contain", "100 of 100");
-    cy.get(
-      ".progress__principle a[href$='/chapter/success_criteria_level_aa'] + .progress__part"
-    ).should("contain", "48 of 52");
-    cy.get(
-      ".progress__principle a[href$='/chapter/success_criteria_level_aaa'] + .progress__part"
-    ).should("contain", "22 of 92");
-    cy.get(
-      ".progress__principle a[href$='/chapter/functional_performance_criteria'] + .progress__part"
-    ).should("contain", "9 of 9");
-    cy.get(
-      ".progress__principle a[href$='/chapter/hardware'] + .progress__part"
-    ).should("contain", "0 of 55");
-    cy.get(
-      ".progress__principle a[href$='/chapter/software'] + .progress__part"
-    ).should("contain", "0 of 26");
-    cy.get(
-      ".progress__principle a[href$='/chapter/support_documentation_and_services'] + .progress__part"
-    ).should("contain", "4 of 5");
   });
 });
