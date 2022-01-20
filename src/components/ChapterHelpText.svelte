2fb1cb544c98529b2dd904a628e9fc8601958d98
Jacqueline Quintanilla <hello@qjac.net>
2022-01-20 09:36:59 -0600

    Create ChapterHelpText component
    Also create yaml file for help text for each chapter.

---
+++ src/components/ChapterHelpText.svelte
@@ -0,0 +1,8 @@
+<script>
+    import chapterHelpText from "../data/chapterHelpText.yaml";
+    export let chapterId;
+</script>
+
+{#if chapterHelpText[chapterId]}
+    <p>{chapterHelpText[chapterId]}</p>
+{/if}
