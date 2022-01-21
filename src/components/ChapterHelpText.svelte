9d5c511c17f0ae891e78fa6b808933bf316d789e
Jacqueline Quintanilla <hello@qjac.net>
2022-01-21 17:13:32 -0600

    261 add chapter help text (#35)
    * Create ChapterHelpText component
    
    Also create yaml file for help text for each chapter.
    
    * Add chapter help text to chapters and about page
    
    * Add links to chapter help message content
    
    Update ChapterHelpText component to render html to properly display links coming from the yaml file.
    
    * Update text content
    
    * Add test to check for existance of chapter help text
    
    * Update text

---
+++ src/components/ChapterHelpText.svelte
@@ -0,0 +1,8 @@
+<script>
+    import chapterHelpText from "../data/chapterHelpText.yaml";
+    export let chapterId;
+</script>
+
+{#if chapterHelpText[chapterId]}
+    <p class="chapter-help-text">{@html chapterHelpText[chapterId]}</p>
+{/if}
