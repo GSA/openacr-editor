1f0751f7450190eab804367ad19e9a29ddf8bad8
Jacqueline Quintanilla <hello@qjac.net>
2022-01-27 11:42:02 -0600

    Move chapter help text to svelte file. Delete the yaml file

--- src/components/ChapterHelpText.svelte
+++ src/components/ChapterHelpText.svelte
@@ -1,21 +1,65 @@
 <script>
-    import chapterHelpText from "../data/chapterHelpText.yaml";
-    export let chapterId;
+  export let chapterId;
 
-    // Target only links <a> inside .chapter-help-text.
-    function openAccordion() {
-        const nonTextContentHeader = document.getElementById("1.1.1-editor");
-        console.log(nonTextContentHeader);
-        // if (nonTextContentHeader.parentElement.parentElement.nodeName === "DETAILS") {
-        //     nonTextContentHeader.parentElement.parentElement.setAttribute("open", true);
-        // }
-    }
+  // Target only links <a> inside .chapter-help-text.
+  function openAccordion() {
+    const nonTextContentHeader = document.getElementById("1.1.1-editor");
+    console.log(nonTextContentHeader);
+    // if (nonTextContentHeader.parentElement.parentElement.nodeName === "DETAILS") {
+    //     nonTextContentHeader.parentElement.parentElement.setAttribute("open", true);
+    // }
+  }
 </script>
 
-{#if chapterId === "success_criteria_level_a"}
-    <p class="chapter-help-text">
-        Provide information about Level A issues with your website or web application. Currently OpenACR only supports WCAG 2.0. The default is web-based, and there are optional fields for <a href="#1.1.1-electronic-docs-editor" on:click="{openAccordion()}">Documentation</a>, <a href="#1.1.1-authoring-tool-editor">Authoring</a> and <a href="#1.1.1-software-editor">Software</a>. Fill out as appropriate.
-    </p>
-{:else if chapterHelpText[chapterId]}
-    <p class="chapter-help-text">{@html chapterHelpText[chapterId]}</p>
+{#if chapterId}
+  <p class="chapter-help-text">
+    {#if chapterId === "about"}
+      Fill out general information about your product or service. Please provide
+      as much detail as possible. By describing your product, the webform will
+      update to show only the sections you need to fill out. If you need to
+      change this later, change your response product information questions.
+    {:else if chapterId === "success_criteria_level_a"}
+      Provide information about Level A issues with your website or web
+      application. Currently OpenACR only supports WCAG 2.0. The default is
+      web-based, and there are optional fields for <a
+        href="#1.1.1-electronic-docs-editor"
+        on:click={openAccordion()}>Documentation</a
+      >, <a href="#1.1.1-authoring-tool-editor">Authoring</a> and
+      <a href="#1.1.1-software-editor">Software</a>. Fill out as appropriate.
+    {:else if chapterId === "success_criteria_level_aa"}
+      Provide information about how your website or web application fails to
+      meet Level AA requirements. Currently OpenACR only supports WCAG 2.0. The
+      default is web-based, and there are optional fields for <a
+        href="#1.2.4-electronic-docs-editor">Documentation</a
+      >, <a href="#1.2.4-authoring-tool-editor">Authoring</a> and
+      <a href="#1.2.4-software-editor">Software</a>. Fill out as appropriate.
+    {:else if chapterId === "success_criteria_level_aaa"}
+      This section is optional. Consider providing information about Level AAA
+      support in your website or web application. WCAG Level AAA reflect
+      accessibility features that exceed the minimum requirements of Section
+      508. It is useful to describe how your product or service exceeds Level AA
+      requirements. Additionally, some end-users and some projects may need
+      specific AAA requirements to be meet.
+    {:else if chapterId === "functional_performance_criteria"}
+      Describe barriers here if they are not covered in other sections.
+      Highlight the impact on different types of disabilities. Optionally, this
+      section may be used to summarize accessibility features described in more
+      detail elsewhere.
+    {:else if chapterId === "hardware"}
+      If the Information and Communications Technology (ICT) includes hardware,
+      please fill out this section.
+    {:else if chapterId === "software"}
+      If your software product is a web application or is rendered through a web
+      browser, do not fill out this section. Fill out <a
+        href="success_criteria_level_a">Tables 1, 2, and 3</a
+      >. Fill out this section if your software product is not web-based.
+      Desktop applications are a common example of software which is not
+      web-based. Software which uses internet communication but is not used via
+      a third-party web browser (e.g., Chrome, Edge, Safari), is not web-based
+      software.
+    {:else if chapterId === "support_documentation_and_services"}
+      If the support documentation for your product is not web-based (e.g., MS
+      Word, OpenOffice or PDF files), fill out this section.
+    {/if}
+  </p>
 {/if}
