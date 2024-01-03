9b6302da9488e5956ad28fa285cb9406cdd81824
Thibaud Colas <thibaudcolas@gmail.com>
2024-01-03 08:16:59 +0000

    Fix extra space in footer links in license

--- src/utils/license.js
+++ src/utils/license.js
@@ -7,7 +7,7 @@ export function license(evaluation, templateType) {
       return sanitizeHtml(
         `<a href="${spdxLicenseList[evaluation.license].url}" target="_blank">${
           spdxLicenseList[evaluation.license].name
-        } <span class="visuallyhidden">(opens in a new window or tab)</span></a>`
+        }<span class="visuallyhidden"> (opens in a new window or tab)</span></a>`
       );
     } else {
       return `[${spdxLicenseList[evaluation.license].name}](${
