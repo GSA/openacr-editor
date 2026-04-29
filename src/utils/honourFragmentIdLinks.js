6097cddf9f24b20c472611ab51c4374ecc686376
Andrew Nielson <29068131+drewnielson@users.noreply.github.com>
2026-04-29 10:28:30 -0400

    Fix cypress failure

--- src/utils/honourFragmentIdLinks.js
+++ src/utils/honourFragmentIdLinks.js
@@ -1,33 +1,42 @@
 export function honourFragmentIdLinks(routerLocation) {
   if (routerLocation.hash) {
-    const fragment = document.querySelector(
-      `[id='${routerLocation.hash.replace("#", "")}']`,
-    );
+    const fragmentSelector = `[id='${routerLocation.hash.replace("#", "")}']`;
 
-    if (!fragment) {
-      return;
+    function focusFragment(attemptsRemaining = 10) {
+      const fragment = document.querySelector(fragmentSelector);
+
+      if (!fragment) {
+        if (attemptsRemaining > 0) {
+          window.requestAnimationFrame(() => {
+            focusFragment(attemptsRemaining - 1);
+          });
+        }
+        return;
+      }
+
+      // if inside a collapsed section, open it.
+      if (
+        fragment.parentElement &&
+        fragment.parentElement.nodeName === "DETAILS"
+      ) {
+        fragment.parentElement.setAttribute("open", "");
+      }
+
+      // explicitly move focus
+      fragment.tabIndex = "-1";
+      fragment.focus();
+
+      // center in viewport
+      fragment.scrollIntoView({
+        behavior: "auto",
+        block: "center",
+      });
+
+      // by setting location.hash explicitly, we ensure :target
+      // selectors in CSS will work as expected
+      window.location.hash = routerLocation.hash;
     }
 
-    // if inside a collapsed section, open it.
-    if (
-      fragment.parentElement &&
-      fragment.parentElement.nodeName === "DETAILS"
-    ) {
-      fragment.parentElement.setAttribute("open", "");
-    }
-
-    // explicitly move focus
-    fragment.tabIndex = "-1";
-    fragment.focus();
-
-    // center in viewport
-    fragment.scrollIntoView({
-      behavior: "auto",
-      block: "center",
-    });
-
-    // by setting location.hash explicitly, we ensure :target
-    // selectors in CSS will work as expected
-    window.location.hash = routerLocation.hash;
+    focusFragment();
   }
 }
